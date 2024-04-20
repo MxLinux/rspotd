472ea7381715e99d7332a0cf8dd40a0fb5bd55c9
Shea Zerda <shea@vulpes.club>
2024-04-20 12:25:52 -0400

    Updated tests to reflect changed return values of functions

--- src/tests.rs
+++ src/tests.rs
@@ -2,17 +2,17 @@ use crate::{generate, generate_multiple, seed_to_des, vals::DEFAULT_SEED};
 
 #[test]
 fn default_seed() {
-    assert_eq!(generate("2021-12-25", DEFAULT_SEED), "ZCARK8TPK5");
+    assert_eq!(generate("2021-12-25", DEFAULT_SEED).unwrap(), "ZCARK8TPK5");
 }
 
 #[test]
 fn custom_seed() {
-    assert_eq!(generate("2021-12-25", "1122AABB"), "FEWNX8OS0O");
+    assert_eq!(generate("2021-12-25", "1122AABB").unwrap(), "FEWNX8OS0O");
 }
 
 #[test]
 fn different_date() {
-    assert_eq!(generate("1960-10-22", DEFAULT_SEED), "WGAR88TPKS");
+    assert_eq!(generate("1960-10-22", DEFAULT_SEED).unwrap(), "WGAR88TPKS");
 }
 
 #[test]
@@ -26,54 +26,42 @@ fn small_multiple() {
     comparison_map.insert(date_begin, seed_begin);
     comparison_map.insert(date_end, seed_end);
     assert_eq!(
-        generate_multiple("2021-12-25", "2021-12-26", DEFAULT_SEED),
+        generate_multiple("2021-12-25", "2021-12-26", DEFAULT_SEED).unwrap(),
         comparison_map
     );
 }
 
 #[test]
 fn des_test() {
-    assert_eq!("3F.94.E2.AA.46.63.AA.78", seed_to_des("ABCD"));
+    assert_eq!("3F.94.E2.AA.46.63.AA.78", seed_to_des("ABCD").unwrap());
 }
 
 #[test]
-#[should_panic]
 fn small_multiple_invalid_range() {
-    println!(
-        "{:?}",
-        generate_multiple("2021-12-26", "2021-12-25", DEFAULT_SEED)
-    );
+    assert!(generate_multiple("2021-12-26", "2021-12-25", DEFAULT_SEED).is_err());
 }
 
 #[test]
-#[should_panic]
 fn multiple_exceed_year() {
-    println!(
-        "{:?}",
-        generate_multiple("2021-12-25", "2022-12-26", DEFAULT_SEED)
-    );
+    assert!(generate_multiple("2021-12-25", "2022-12-26", DEFAULT_SEED).is_err());
 }
 
 #[test]
-#[should_panic]
 fn small_seed() {
-    generate("1960-10-22", "ABC");
+    assert!(generate("1960-10-22", "ABC").is_err());
 }
 
 #[test]
-#[should_panic]
 fn large_seed() {
-    generate("1960-10-22", "ABCABCABC");
+    assert!(generate("1960-10-22", "ABCABCABC").is_err());
 }
 
 #[test]
-#[should_panic]
 fn invalid_date() {
-    generate("Phoenix dactylifera", DEFAULT_SEED);
+    assert!(generate("Phoenix dactylifera", DEFAULT_SEED).is_err());
 }
 
 #[test]
-#[should_panic]
 fn des_seed_short() {
-    seed_to_des("ABC");
+    assert!(seed_to_des("ABC").is_err());
 }
